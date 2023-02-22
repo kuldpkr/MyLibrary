3a6e1b44a75d8cf8d37b37bdc471a941e0d4e37f
Kuldeep Kumar <kuldpkr@hotmail.com>
2023-02-22 16:24:54 +0530

    Created Book create/index/new routes

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV !== 'production') {
   
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
-  
+  const bookRouter = require('./routes/books')
+
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
@@ -25,5 +26,6 @@ if (process.env.NODE_ENV !== 'production') {
   
   app.use('/', indexRouter)
   app.use('/authors', authorRouter)
-  
+  app.use('/books', bookRouter)
+
   app.listen(process.env.PORT || 3000)
